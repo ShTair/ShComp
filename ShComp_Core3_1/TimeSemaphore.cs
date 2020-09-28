7c2523d49ff90a3dce0eb1ed70111654af074cf6
ShTair <git@vsh.jp>
2020-09-28 16:06:01 +0900

    プロジェクト ファイルを追加します。

---
+++ ShComp_Core3_1/TimeSemaphore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShComp
+{
+    class TimeSemaphore
+    {
+        private SemaphoreSlim _sem;
+
+        public TimeSpan Period { get; }
+
+        public int CurrentCount => _sem.CurrentCount;
+
+        public TimeSemaphore(int count, TimeSpan period)
+        {
+            _sem = new SemaphoreSlim(count);
+            Period = period;
+        }
+
+        public async Task WaitAsync()
+        {
+            await _sem.WaitAsync();
+            _ = Task.Delay(Period).ContinueWith(_ => _sem.Release());
+        }
+    }
+}

b4b9b3d0561a82d1ac8932131c592d30be201e9d
ShTair <git@vsh.jp>
2022-01-05 12:35:54 +0900

    設定用のメソッドを追加

---
+++ ShComp/DependencyInjection/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ShComp.DependencyInjection;
+
+public static class Extensions
+{
+    public static void ConfigureUsingOptions<T>(this IServiceCollection services, IConfiguration configuration)
+    {
+        var parentType = typeof(T);
+        if (parentType.GetNestedType("Options") is not { } optsType) throw new Exception();
+
+        var configMethod = typeof(OptionsConfigurationServiceCollectionExtensions)
+            .GetMethod("Configure", new[] { typeof(IServiceCollection), typeof(IConfiguration) })!
+            .MakeGenericMethod(optsType);
+
+        configMethod.Invoke(null, new object[] { services, configuration.GetSection(parentType.Name) });
+    }
+
+    public static void AddSingletonWithConfiguration<T>(this IServiceCollection services, IConfiguration configuration) where T : class
+    {
+        services.ConfigureUsingOptions<T>(configuration);
+        services.AddSingleton<T>();
+    }
+
+    public static void AddHostedServiceWithConfiguration<T>(this IServiceCollection services, IConfiguration configuration) where T : class, IHostedService
+    {
+        services.ConfigureUsingOptions<T>(configuration);
+        services.AddHostedService<T>();
+    }
+}

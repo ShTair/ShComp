76cb0195c2431460077c2a0e8a5a7a5958624b76
ShTair <git@vsh.jp>
2022-01-13 16:26:58 +0900

    usingでロックするためのメソッドを追加

--- ShComp/Threading/AsyncLock.cs
+++ ShComp/Threading/AsyncLock.cs
@@ -45,4 +45,28 @@ public readonly struct AsyncLock
         try { return await func(); }
         finally { _semaphore.Release(); }
     }
+
+    public readonly async Task<Releaser> WaitAsync()
+    {
+        await _semaphore.WaitAsync();
+        return new Releaser(_semaphore);
+    }
+
+    /// <summary>
+    /// Disposeを呼び出すことでロックを開放します。Disposeは絶対に1回しか呼び出さないでください。
+    /// </summary>
+    public readonly struct Releaser : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore;
+
+        public Releaser(SemaphoreSlim semaphore)
+        {
+            _semaphore = semaphore;
+        }
+
+        public readonly void Dispose()
+        {
+            _semaphore.Release();
+        }
+    }
 }

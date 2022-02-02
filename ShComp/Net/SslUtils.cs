69a423e956d14231b612594593fc540daaa65991
ShTair <git@vsh.jp>
2022-02-02 14:12:22 +0900

    証明書を取得するコードを追加

---
+++ ShComp/Net/SslUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ShComp.Net;
+
+public static class SslUtils
+{
+    public static async Task<X509Certificate2?> GetCertificateAsync(string host, int port = 443, CancellationToken cancellationToken = default)
+    {
+        using var client = new TcpClient(host, port);
+        using var stream = client.GetStream();
+        using var sslStream = new SslStream(stream);
+
+        var options = new SslClientAuthenticationOptions
+        {
+            TargetHost = host,
+            RemoteCertificateValidationCallback = (_, _, _, _) => true,
+        };
+
+        await sslStream.AuthenticateAsClientAsync(options, cancellationToken);
+        return sslStream.RemoteCertificate as X509Certificate2;
+    }
+}

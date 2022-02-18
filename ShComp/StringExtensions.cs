b611f8d50149aa09a8e9e36624cd0ce216609c7f
ShTair <git@vsh.jp>
2022-02-18 19:43:48 +0900

    文字列の変換メソッドを実装

---
+++ ShComp/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ShComp;
+
+public static class StringExtensions
+{
+    public static int? AsInt(this string s) => int.TryParse(s, out var result) ? result : null;
+
+    public static double? AsDouble(this string s) => double.TryParse(s, out var result) ? result : null;
+
+    public static DateTime? AsDateTime(this string s) => DateTime.TryParse(s, out var result) ? result : null;
+
+    public static DateOnly? AsDate(this string s) => DateTime.TryParse(s, out var result) ? DateOnly.FromDateTime(result) : null;
+
+    public static DateOnly? AsDateOnly(this string s) => DateOnly.TryParse(s, out var result) ? result : null;
+}

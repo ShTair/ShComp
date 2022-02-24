439bd4d9288cbb063509b83d907708d9ff04b4ee
ShTair <git@vsh.jp>
2022-02-24 12:13:30 +0900

    リングバッファを実装

---
+++ ShComp.Test/RingBufferTest.cs
@@ -0,0 +1,48 @@
+﻿using ShComp.Collections;
+using System.Linq;
+using Xunit;
+
+namespace ShComp.Test;
+
+public class RingBufferTest
+{
+    [Fact]
+    public void CountTest()
+    {
+        var buffer = new RingBuffer<int?>(3);
+        buffer.Add(1);
+        Assert.Single(buffer);
+
+        buffer.Add(null);
+        buffer.Add(2);
+        Assert.Equal(3, buffer.Count);
+
+        buffer.Add(null);
+        buffer.Add(3);
+        Assert.Equal(3, buffer.Count);
+    }
+
+    [Fact]
+    public void EnumerableTest()
+    {
+        var buffer = new RingBuffer<int>(3) { 1, 2, 3, 4, 5 };
+        var items = buffer.ToArray();
+        Assert.Equal(4, items[1]);
+    }
+
+    [Fact]
+    public void RemoveTest()
+    {
+        var buffer = new RingBuffer<int>(3) { 1, 2, 3, 4, 5 };
+        buffer.Remove();
+        Assert.Equal(2, buffer.Count);
+        Assert.Equal(4, buffer[3]);
+        Assert.Equal(4, buffer.ToArray()[0]);
+
+        buffer.Remove();
+        Assert.True(buffer.Remove());
+        Assert.Empty(buffer);
+
+        Assert.False(buffer.Remove());
+    }
+}
---
+++ ShComp/Collections/RingBuffer.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+
+namespace ShComp.Collections;
+
+public class RingBuffer<T> : IList<T>
+{
+    private readonly T?[] _buffer;
+
+    private int _nextIndex;
+    private int _count;
+
+    public int HeadIndex => _nextIndex - _count;
+
+    public RingBuffer(int capacity)
+    {
+        _buffer = new T[capacity];
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < _nextIndex - _count || index >= _nextIndex) throw new ArgumentOutOfRangeException(nameof(index));
+            return _buffer[index % _buffer.Length]!;
+        }
+        set => throw new NotImplementedException();
+    }
+
+    public int Count => _count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T item)
+    {
+        _buffer[_nextIndex % _buffer.Length] = item;
+        _nextIndex++;
+        if (_count < _buffer.Length) _count++;
+    }
+
+    public void Clear()
+    {
+        Array.Clear(_buffer, 0, _buffer.Length);
+        _nextIndex = 0;
+        _count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        for (var i = 0; i < _count && i < array.Length - arrayIndex; i++)
+        {
+            array[arrayIndex + i] = this[HeadIndex + i];
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = 0; i < _count; i++)
+        {
+            yield return _buffer[(_nextIndex + i) % _buffer.Length]!;
+        }
+    }
+
+    public int IndexOf(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove()
+    {
+        if (_count <= 0) return false;
+        _buffer[(_nextIndex - _count) % _buffer.Length] = default;
+        _count--;
+        return true;
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}

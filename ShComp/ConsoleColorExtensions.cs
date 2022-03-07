be398fea19682ab9ec792867c8c472a323d91fc4
ShTair <git@vsh.jp>
2022-03-07 12:40:51 +0900

    コンソールの色を変更する拡張メソッドを追加

---
+++ ShComp/ConsoleColorExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace ShComp;
+
+public static class ConsoleColorExtensions
+{
+    public static ConsoleColorRestorer SetForeground(this ConsoleColor color)
+    {
+        var restorer = new ConsoleColorRestorer();
+        Console.ForegroundColor = color;
+        return restorer;
+    }
+
+    public static ConsoleColorRestorer SetBackground(this ConsoleColor color)
+    {
+        var restorer = new ConsoleColorRestorer();
+        Console.BackgroundColor = color;
+        return restorer;
+    }
+}
+
+public sealed class ConsoleColorRestorer : IDisposable
+{
+    private readonly (ConsoleColor, ConsoleColor) _defaults;
+
+    public ConsoleColorRestorer()
+    {
+        _defaults = (Console.ForegroundColor, Console.BackgroundColor);
+    }
+
+    public void Dispose()
+    {
+        (Console.ForegroundColor, Console.BackgroundColor) = _defaults;
+    }
+}

6b8c8aec8b31c8f2b0972ffb05f5c9c38c019b37
ShTair <git@vsh.jp>
2022-03-07 12:37:46 +0900

    配列をシャッフルする拡張メソッドを追加

---
+++ ShComp/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ShComp;
+
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// 指定した配列の中身をランダムな順序で返します。<br />
+    /// 指定した配列そのものの順序も変更されます。
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IList<T> items)
+    {
+        var random = new Random();
+        for (int i = 0; i < items.Count; i++)
+        {
+            var r = random.Next(i, items.Count);
+            if (i != r) (items[i], items[r]) = (items[r], items[i]);
+            yield return items[i];
+        }
+    }
+
+    /// <summary>
+    /// 指定したコレクションの中身をランダムな順序で返します。<br />
+    /// 内部的に配列を生成しています。
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> items)
+    {
+        return Shuffle(items.ToArray());
+    }
+}

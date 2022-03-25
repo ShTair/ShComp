c43aa704ca46b855bb229e4d6be2512664b9f743
ShTair <git@vsh.jp>
2022-03-25 17:55:39 +0900

    ドキュメンテーションコメントを追加

--- ShComp/StringExtensions.cs
+++ ShComp/StringExtensions.cs
@@ -8,7 +8,13 @@ public static class StringExtensions
 
     public static DateTime? AsDateTime(this string s) => DateTime.TryParse(s, out var result) ? result : null;
 
+    /// <summary>
+    /// DateTimeとして読み込み、DateOnlyに変換して返します。
+    /// </summary>
     public static DateOnly? AsDate(this string s) => DateTime.TryParse(s, out var result) ? DateOnly.FromDateTime(result) : null;
 
+    /// <summary>
+    /// 文字列に時間が含まれている場合、失敗します。
+    /// </summary>
     public static DateOnly? AsDateOnly(this string s) => DateOnly.TryParse(s, out var result) ? result : null;
 }

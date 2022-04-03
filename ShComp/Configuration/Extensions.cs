8bd9eec1506b36ddb52d8c82eaa9f9dff7ff25ac
ShTair <git@vsh.jp>
2022-04-03 23:33:39 +0900

    2つの文字列配列をキーと値として構成に読み込む

---
+++ ShComp/Configuration/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using ShComp.Configuration;
+
+namespace Microsoft.Extensions.Configuration;
+
+public static class Extensions
+{
+    public static IConfigurationBuilder AddStringArray(this IConfigurationBuilder builder, IEnumerable<string> keys, IEnumerable<string> values)
+    {
+        return builder.Add(new StringArrayConfigurationSource(keys, values));
+    }
+}
---
+++ ShComp/Configuration/StringArrayConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ShComp.Configuration;
+
+public class StringArrayConfigurationSource : IConfigurationSource
+{
+    private readonly IEnumerable<string> _keys;
+    private readonly IEnumerable<string> _values;
+
+    public StringArrayConfigurationSource(IEnumerable<string> keys, IEnumerable<string> values)
+    {
+        _keys = keys;
+        _values = values;
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new StringArrayConfigurationProvider(_keys, _values);
+    }
+}
+
+public class StringArrayConfigurationProvider : ConfigurationProvider
+{
+    private readonly IEnumerable<string> _keys;
+    private readonly IEnumerable<string> _values;
+
+    public StringArrayConfigurationProvider(IEnumerable<string> keys, IEnumerable<string> values)
+    {
+        _keys = keys;
+        _values = values;
+    }
+
+    public override void Load()
+    {
+        foreach (var (key, value) in _keys.Zip(_values, (key, value) => (key, value)))
+        {
+            Data[key] = value;
+        }
+    }
+}

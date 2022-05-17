62d7e2456171c62a17d5c38c7b55688c0dd45da3
ShTair <git@vsh.jp>
2022-05-17 13:37:38 +0900

    DIの設定読み込みの拡張メソッドを修正

--- ShComp/DependencyInjection/Extensions.cs
+++ ShComp/DependencyInjection/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 namespace ShComp.DependencyInjection;
 
@@ -8,14 +9,29 @@ public static class Extensions
 {
     public static void ConfigureUsingOptions<T>(this IServiceCollection services, IConfiguration configuration)
     {
-        var parentType = typeof(T);
-        if (parentType.GetNestedType("Options") is not { } optsType) throw new Exception();
-
         var configMethod = typeof(OptionsConfigurationServiceCollectionExtensions)
-            .GetMethod("Configure", new[] { typeof(IServiceCollection), typeof(IConfiguration) })!
-            .MakeGenericMethod(optsType);
+            .GetMethod("Configure", new[] { typeof(IServiceCollection), typeof(IConfiguration) })!;
 
-        configMethod.Invoke(null, new object[] { services, configuration.GetSection(parentType.Name) });
+        var parentType = typeof(T);
+        var types = parentType.GetNestedTypes();
+
+        foreach (var type in types)
+        {
+            string? sectionName = null;
+            if (type.GetCustomAttribute<OptionsAttribute>() is { } optionsAttribute)
+            {
+                sectionName = optionsAttribute.SectionName ?? parentType.Name;
+            }
+            else if (type.Name == "Options")
+            {
+                sectionName = parentType.Name;
+            }
+
+            if (sectionName is null) continue;
+
+            var genericConfigMethod = configMethod.MakeGenericMethod(type);
+            genericConfigMethod.Invoke(null, new object[] { services, configuration.GetSection(sectionName) });
+        }
     }
 
     public static void AddSingletonWithConfiguration<T>(this IServiceCollection services, IConfiguration configuration) where T : class
@@ -29,4 +45,10 @@ public static class Extensions
         services.ConfigureUsingOptions<T>(configuration);
         services.AddHostedService<T>();
     }
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class OptionsAttribute : Attribute
+{
+    public string? SectionName { get; set; }
 }

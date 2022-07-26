44b9ca0e140e1552f2c495096b7e430810536a37
ShTair <git@vsh.jp>
2022-07-26 14:50:00 +0900

    時間表示のフォーマットを指定できるように変更

--- ShComp/IO/TimeTextWriter.cs
+++ ShComp/IO/TimeTextWriter.cs
@@ -6,14 +6,19 @@ public class TimeTextWriter : TextWriter
 {
     private bool _isBeginningOfLine;
 
+    private readonly string _format;
+
     public TextWriter BaseWriter { get; }
 
     public override Encoding Encoding => BaseWriter.Encoding;
 
-    public TimeTextWriter(TextWriter baseWriter)
+    public TimeTextWriter(TextWriter baseWriter) : this(baseWriter, "HH:mm:ss.f ") { }
+
+    public TimeTextWriter(TextWriter baseWriter, string format)
     {
         _isBeginningOfLine = true;
         BaseWriter = baseWriter;
+        _format = format;
     }
 
     public override void Write(char value)
@@ -25,15 +30,15 @@ public class TimeTextWriter : TextWriter
         else if (_isBeginningOfLine)
         {
             _isBeginningOfLine = false;
-            BaseWriter.Write($"{DateTime.Now:HH:mm:ss.f} ");
+            BaseWriter.Write(DateTime.Now.ToString(_format));
         }
 
         BaseWriter.Write(value);
     }
 
-    public static void SetOut()
+    public static void SetOut(string format = "HH:mm:ss.f ")
     {
-        var writer = new TimeTextWriter(Console.Out);
+        var writer = new TimeTextWriter(Console.Out, format);
         Console.SetOut(writer);
     }
 }

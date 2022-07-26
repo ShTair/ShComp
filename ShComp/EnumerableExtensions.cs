69585c8cfbeb820971efcc74b95aa21555b293e8
ShTair <git@vsh.jp>
2022-07-26 14:49:22 +0900

    指定した個数のみを返すシャッフルのメソッドを追加

--- ShComp/EnumerableExtensions.cs
+++ ShComp/EnumerableExtensions.cs
@@ -3,18 +3,36 @@
 public static class EnumerableExtensions
 {
     /// <summary>
-    /// 指定した配列の中身をランダムな順序で返します。<br />
+    /// 指定した配列の中身を、指定した個数だけランダムな順序で返します。<br />
     /// 指定した配列そのものの順序も変更されます。
     /// </summary>
-    public static IEnumerable<T> Shuffle<T>(this IList<T> items)
+    public static IEnumerable<T> Shuffle<T>(this IList<T> items, int count)
     {
         var random = new Random();
-        for (int i = 0; i < items.Count; i++)
+        for (int i = 0; i < count; i++)
         {
             var r = random.Next(i, items.Count);
             if (i != r) (items[i], items[r]) = (items[r], items[i]);
             yield return items[i];
         }
+    }
+
+    /// <summary>
+    /// 指定した配列の中身をランダムな順序で返します。<br />
+    /// 指定した配列そのものの順序も変更されます。
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IList<T> items)
+    {
+        return Shuffle(items, items.Count);
+    }
+
+    /// <summary>
+    /// 指定したコレクションの中身を、指定した個数だけランダムな順序で返します。<br />
+    /// 内部的に配列を生成しています。
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> items, int count)
+    {
+        return Shuffle(items.ToArray(), count);
     }
 
     /// <summary>

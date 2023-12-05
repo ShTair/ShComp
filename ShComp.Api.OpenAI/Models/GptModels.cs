1f2473175d7c9b3793cfc5744ca7c61de003b1d4
ShTair <git@vsh.jp>
2023-12-05 09:05:31 +0900

    gptのモデルを追加

--- ShComp.Api.OpenAI/Models/GptModels.cs
+++ ShComp.Api.OpenAI/Models/GptModels.cs
@@ -5,4 +5,6 @@ public class GptModels
     public const string Gpt3_5Turbo = "gpt-3.5-turbo";
 
     public const string Gpt4 = "gpt-4";
+
+    public const string Gpt4Turbo = "gpt-4-1106-preview";
 }
